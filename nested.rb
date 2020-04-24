64c01b06005a8716b4d2be6a963633458f7cb6da
Evan Burton <evan@heyevan.com>
2020-04-24 02:39:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,8 +129,8 @@ def adding_to_dennis
         }
      }
 
-additional_language = "Assembly"
-		 programmer_hash[:dennis_ritchie][:languages[1]<<additional_language]
+
+		 programmer_hash[:dennis_ritchie][:languages[1]<<"Assembly"]
 		 programmer_hash
 
 end
